1541e7697a85423784ea267ede0ceb8010c7ae29
OTOfngn <c98423795274152963@outlook.com>
2026-02-27 10:03:36 +0200

    local storage added

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 console.log("js connected");
 
+// page refresh
+function refresh() {
+    window.location.reload();
+}
+
+// data storage
 
 // Theme button
 const themeBtn = document.getElementById("theme-btn");
@@ -16,22 +22,39 @@ themeBtn.onclick = function () {
 };
 
 // Booking button template
-function createBookingBtn(text = "Book") {
-    const btn = document.createElement("button");
-    btn.textContent = text;
-    let if_booked = false;
+function createBookingBtn(position = 0) {
+    const btn = document.createElement("a");
+    const key = "booking_" + position;
+    // btn.href = "#";
+    if (localStorage.getItem(key) === null) {
+        localStorage.setItem(key, 'available');
+    }
+    let if_booked = localStorage.getItem(key);
+    btn.textContent = if_booked === 'booked' ? "Booked" : 'Book';
+    if (if_booked === 'booked') btn.style.color = "var(--primary-color)";
+
     btn.onclick = function () {
-        if (if_booked) {
-            btn.textContent = "Book";
-            if_booked = false;
-        } else {
-            btn.textContent = "Booked";
-            if_booked = true;
+        switch (if_booked) {
+            case 'booked':
+                btn.textContent = "Book";
+                btn.style.color = "var(--text-color)";
+                if_booked = 'available';
+                break;
+            case 'available':
+                btn.textContent = "Booked";
+                btn.style.color = "var(--primary-color)";
+                if_booked = 'booked';
+                break;
+            case 'booked by someone else':
+                alert("This is booked by someone else.");
+                break;
         }
+        localStorage.setItem(key, if_booked);
     };
     return btn;
 }
 
 // Fill empty table cells with booking buttons
 const cells = document.querySelectorAll("tbody td:not(:first-child)");
-cells.forEach(cell => { cell.appendChild(createBookingBtn()); });
+cells.forEach((cell, index) => { cell.appendChild(createBookingBtn(index)); });
+// localStorage.clear();
